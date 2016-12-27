476607f86547a8b96fd6c52dfbd722c568c1a281
david amick <github@davidamick.com>
2016-12-27 12:38:35 -0800

    Change to FROM scratch

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
-FROM ubuntu:16.04
+FROM scratch
 MAINTAINER david amick <docker@davidamick.com>
 
+ADD ca-certificates.crt /etc/ssl/certs/
 ADD proxy-http-status-code-only /
 
 ENTRYPOINT ["/proxy-http-status-code-only"]
--- build-and-push.sh
+++ build-and-push.sh
@@ -2,10 +2,21 @@
 
 set -e
 
-docker run --rm -v "$PWD":/usr/src/proxy-http-status-code-only -w /usr/src/proxy-http-status-code-only golang:1.7 bash -c "go get github.com/tools/godep && godep restore && go build -v"
+# Build the binary
+docker run -it --rm -v "$PWD":/go/src/proxy-http-status-code-only -w /go/src/proxy-http-status-code-only golang:1.7 bash -c "go get github.com/tools/godep && godep restore -v && CGO_ENABLED=0 GOOS=linux go build -a -v"
 chmod ug+x proxy-http-status-code-only
 
+# Get a recent copy of ca-certificates.crt
+docker pull ubuntu:latest
+docker build -f Dockerfile.certs -t temp-certs:latest .
+ID=$(docker run -d temp-certs:latest)
+docker cp $ID:/etc/ssl/certs/ca-certificates.crt .
+docker rm $ID
+docker rmi temp-certs:latest
+
 docker build -t snarlysodboxer/proxy-http-status-code-only:latest .
+
+rm proxy-http-status-code-only
 
 docker push snarlysodboxer/proxy-http-status-code-only:latest
 
